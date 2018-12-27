6afee344d8255ac03f5c4d8de457e4f785c7d468
Kane Clover <kane@assure24.com>
2018-12-27 23:45:59 +0000

    Draw markers on the map from firebase data

--- containers/PlanningMap.js
+++ containers/PlanningMap.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, ScrollView, StyleSheet, Text, Button } from 'react-native'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 
 const styles = StyleSheet.create({
   container: {
@@ -15,23 +15,27 @@ const styles = StyleSheet.create({
   },
 });
 
-export default class PlanningMap extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <MapView
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={styles.map}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-        >
-        </MapView>
-      </View>
-    )
-  }
-}
+const PlanningMap = ({ markers }) => (
+  <View style={styles.container}>
+    <MapView
+      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+      style={styles.map}
+      region={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      }}
+    >
+      {markers.map((marker, i) => marker.lat ? (
+        <Marker
+          coordinate={{latitude: marker.lat, longitude: marker.lng}}
+          title={marker.title}
+          key={i}
+        />
+      ) : false)}
+    </MapView>
+  </View>
+)
 
+export default PlanningMap
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,32 +4,48 @@ import PlanningMap from '../containers/PlanningMap'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import MenuButton from '../components/MenuOpenButton'
 import { UserContext } from '../App'
+import firebase from "react-native-firebase"
 
 export default class HomeScreen extends React.Component {
-
-  constructor() {
-    super()
-    this.state = {
-      fcmToken: false,
-    }
-  }
-
-
 
   static navigationOptions = {
     headerTitle: 'Home',
     headerLeft: <MenuButton/>
   };
 
+  state = {
+    planningApps: [],
+  }
+
+  /**
+   * Get relevant planning applications
+   */
+  componentDidMount() {
+    const db = firebase.firestore().collection('planningApps');
+
+    let planningApps = []
+
+    db.get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        const app = doc.data();
+        planningApps.push(app)
+      });
+      this.setState({planningApps})
+    });
+  }
+
   render() {
+    const { planningApps } = this.state;
+    const { navigation } = this.props;
+
     return (
       <UserContext.Consumer>
         {({fcmToken}) => (
           <View>
-            <PlanningMap/>
+            <PlanningMap markers={planningApps}/>
             <Button
               title="Go to Details"
-              onPress={() => this.props.navigation.navigate('Details')}
+              onPress={() => navigation.navigate('Details')}
             />
             <Text>fcm token from context: {fcmToken}</Text>
           </View>
