709bce9ae69ea0ef17e5782a1714ad64b4ad90de
Kane Clover <kane@assure24.com>
2018-12-30 21:42:03 +0000

    Set search radius

--- screens/SearchRadiusScreen.js
+++ screens/SearchRadiusScreen.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, TextInput } from 'react-native'
+import { db } from '../App'
 
 export default class SearchRadiusScreen extends React.Component {
 
   constructor() {
-    super()
+    super();
     this.state = {}
   }
 
@@ -12,10 +13,21 @@ export default class SearchRadiusScreen extends React.Component {
     drawerLabel: 'Set search radius',
   };
 
+  async setRadius(radius) {
+    // TODO: Perhaps this needs to happen higher up the hierarchy, so change gets reflected to other components?
+    await db.collection('users').doc(this.props.screenProps.userId).update({
+      searchRadius: radius,
+    })
+  }
+
   render() {
     return (
       <View>
         <Text>Set search radius here</Text>
+        <TextInput
+          keyboardType="numeric"
+          placeholder="Search radius"
+          onChangeText={r => this.setRadius(r)}/>
       </View>
     )
   }
